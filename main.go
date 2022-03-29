e1dd31af7dcdc89f56b40c073ce07541f17683aa
Angga Nur Prasetya <angganurprasetya4@gmail.com>
2022-03-29 09:18:11 +0700

    🏍

--- main.go
+++ main.go
@@ -149,7 +149,7 @@ func CORSMiddleware() gin.HandlerFunc {
         c.Writer.Header().Set("Access-Control-Allow-Origin", getEnv("WHITE_LIST_IP"))
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 
         if c.Request.Method == "OPTIONS" {
             c.AbortWithStatus(204)
